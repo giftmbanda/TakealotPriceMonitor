dfc445388e2bc0e3fa1f6b90ad2914a052f80e89
Gift Banda <giftmbanda@gmail.com>
2020-10-12 21:24:24 +0200

    configuring nodemailer

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
-const puppeteer = require('puppeteer');
-const CronJob = require('cron').CronJob;
-const nodemailer = require('nodemailer');
+const puppeteer = require("puppeteer");
+const CronJob = require("cron").CronJob;
+const nodemailer = require("nodemailer");
 
-const PAGE_URL = 'https://www.takealot.com/dell-se2719hr-69cm-27-fhd-ips-led-monitor/PLID68852194'; // for take alot dell monitor
-
+const url =
+  "https://www.takealot.com/dell-se2719hr-69cm-27-fhd-ips-led-monitor/PLID68852194"; // for take alot dell monitor
 
 scrap = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(PAGE_URL, { waitUntil: 'networkidle0' });
+  await page.goto(url, { waitUntil: "networkidle0" });
 
   let data = await page.evaluate(() => {
-    let name = document.querySelector('div[class="cell auto"] > h1').innerText;
-    let price = document.querySelector('span[class="currency plus currency-module_currency_29IIm"]').innerText;
-    return { name, price };
+    let title = document.querySelector('div[class="cell auto"] > h1').innerText;
+    let price = document.querySelector(
+      'span[class="currency plus currency-module_currency_29IIm"]'
+    ).innerText;
+    return { title, price };
   });
 
   await browser.close();
   return data; /* Returning with the scraped data */
 };
 
-
 startTracking = async (dealPrice) => {
-  let job = new CronJob('*/5 * * * * *', async () => { //runs every 5 seconds in this config for development purpose
-    let { name, price, ...rest } = await scrap();
+  let job = new CronJob("*/5 * * * * *", async () => { //runs every 5 seconds in this config for development purpose
+      let data = await scrap();
+      let { title, price } = data;
 
-    if (await formatPrice(price) <= dealPrice) {
-      // if the item price is equal or less that the my dealPrice then send email
-      return console.log(`Sending an email, The ${name} is now ${price}`);
-    }
-    return console.log(`The ${name} is still ${price}`);
-  }, null, true);
-  job.start();
-}
-
-// format 'R 3,399' to '3399' or 'R 3,399.99' to '3399.99'
-formatPrice = async randPrice => Number(randPrice.replace(/[^0-9.-]+/g, ""));
+      if ((await formatPrice(price)) <= dealPrice) {
+        return console.log(`Sending an email, The ${title} is now ${price}`);
+        // await sendEmail(data); //send email
+      }
+      return console.log(`The ${title} is still ${price}`);
+    }, null, true
+  ); job.start();
+};
 
 // configure email with nodemailer
-// configureEmail() = async () => {}
+sendEmail = async (data) => {
+  let { title, price, ...rest } = data;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: EMAIL, // your gmail address
+      pass: EMAIL_PASSWORD, // your gmail password
+    },
+  });
+  let mailOptions = {
+    subject: `Takealot.com Deal`,
+    to: `giftmbanda@gmail.com`, // recipient email
+    from: `TakealotPriceMonitor`,
+    html: `<h2>Hi there!</h2>
+    <p>The ${title} is now ${price}</p>
+    <p>Please click <a href="">here</a> to reset your password.</p>`,
+  };
+  return transporter.sendMail(mailOptions);
+};
+
+// format 'R 3,399' to '3399' or 'R 3,399.99' to '3399.99'
+formatPrice = async (randPrice) => Number(randPrice.replace(/[^0-9.-]+/g, ""));
 
 // start tracking
-startTracking(300);
-
+startTracking(4400);
