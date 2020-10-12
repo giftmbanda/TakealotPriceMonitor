25c38bdf690af6baa91e627f311153f1f30fe9d0
Gift Banda <giftmbanda@gmail.com>
2020-10-12 04:29:57 +0200

    adding formatPrice() and startTracking() fucntions

--- index.js
+++ index.js
@@ -5,26 +5,41 @@ const nodemailer = require('nodemailer');
 const PAGE_URL = 'https://www.takealot.com/dell-se2719hr-69cm-27-fhd-ips-led-monitor/PLID68852194'; // for take alot dell monitor
 
 
-checkPrice = async () => {
-
-   /* Initiate the Puppeteer browser */
+scrap = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
-  /* Go to the page url and wait for it to load */
   await page.goto(PAGE_URL, { waitUntil: 'networkidle0' });
 
-  /* Run javascript inside of the page */
   let data = await page.evaluate(() => {
     let name = document.querySelector('div[class="cell auto"] > h1').innerText;
-    let randPrice = document.querySelector('span[class="currency plus currency-module_currency_29IIm"]').innerText;
-    //let price = Number(randPrice.replace(/[^0-9.-]+/g,""));
-    return { name, randPrice }; /* Returning with the scraped data */
+    let price = document.querySelector('span[class="currency plus currency-module_currency_29IIm"]').innerText;
+    return { name, price };
   });
 
-  /* Outputting what we scraped */
-  console.log(data);
   await browser.close();
+  return data; /* Returning with the scraped data */
 };
 
-checkPrice();
+
+startTracking = async (dealPrice) => {
+  let job = new CronJob('*/5 * * * * *', async () => { //runs every 5 seconds in this config for development purpose
+    let { name, price, ...rest } = await scrap();
+
+    if (await formatPrice(price) <= dealPrice) {
+      // if the item price is equal or less that the my dealPrice then send email
+      return console.log(`Sending an email, The ${name} is now ${price}`);
+    }
+    return console.log(`The ${name} is still ${price}`);
+  }, null, true);
+  job.start();
+}
+
+// format 'R 3,399' to '3399' or 'R 3,399.99' to '3399.99'
+formatPrice = async randPrice => Number(randPrice.replace(/[^0-9.-]+/g, ""));
+
+// configure email with nodemailer
+// configureEmail() = async () => {}
+
+// start tracking
+startTracking(300);
+
